36b6c8b7f9414645494cde0bde53e1ae01877b28
ZMark <351479188@qq.com>
2024-04-12 22:22:12 +0800

    init

---
+++ src/main/java/org/fdu/awt/minifduuser/MiniFduUserApplication.java
@@ -0,0 +1,13 @@
+package org.fdu.awt.minifduuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniFduUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiniFduUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/fdu/awt/minifduuser/MiniFduUserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fdu.awt.minifduuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniFduUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
