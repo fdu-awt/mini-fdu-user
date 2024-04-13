805f64e8df1fc2693f7ac31c3e1f59582724f416
zmark <351479188@qq.com>
2024-04-14 00:41:16 +0800

    feat:result

---
+++ src/main/java/org/fdu/awt/minifduuser/result/Result.java
@@ -0,0 +1,34 @@
+package org.fdu.awt.minifduuser.result;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author ZMARK
+ * @className Result
+ * @description: 请求结果返回类
+ * @date 2023/3/5 21:38
+ */
+@Data
+@NoArgsConstructor
+public class Result {
+    private int code;
+    /**
+     * 表示Result是否按照协议进行
+     */
+    private boolean uxApi;
+    /**
+     * 请求是否成功
+     */
+    private boolean success;
+    private String msg;
+    private Object object;
+
+    public Result(int code, boolean success, String msg, Object object) {
+        this.code = code;
+        this.uxApi = true;
+        this.success = success;
+        this.msg = msg;
+        this.object = object;
+    }
+}
---
+++ src/main/java/org/fdu/awt/minifduuser/result/ResultFactory.java
@@ -0,0 +1,51 @@
+package org.fdu.awt.minifduuser.result;
+
+/**
+ * @author ZMARK
+ * @className ResultFactory
+ * @description: 请求结果返回类工厂
+ * @date 2023/3/5 21:45
+ */
+public class ResultFactory {
+
+    public static Result buildResult(Integer resultCode, boolean success, String msg, Object data) {
+        return new Result(resultCode, success, msg, data);
+    }
+
+    public static Result buildResult(ResultCode resultCode, boolean success, String msg, Object data) {
+        return new Result(resultCode.getCode(), success, msg, data);
+    }
+
+    public static Result buildSuccessResult(Object data) {
+        return buildResult(ResultCode.SUCCESS, true, "成功", data);
+    }
+
+    public static Result buildSuccessResult() {
+        return buildResult(ResultCode.SUCCESS, true, "成功", null);
+    }
+
+    public static Result buildSuccessResult(String msg, Object data) {
+        return buildResult(ResultCode.SUCCESS, true, msg, data);
+    }
+
+    public static Result buildFailResult(String msg) {
+        return buildResult(ResultCode.FAIL, false, msg, null);
+    }
+
+    public static Result buildInternalServerErrorResult() {
+        return buildResult(ResultCode.INTERNAL_SERVER_ERROR, false, "服务器开小差啦~", null);
+    }
+
+    public static Result buildInsufficientPermissionsResult() {
+        return buildResult(ResultCode.UNAUTHORIZED, false, "您的权限不足！", null);
+    }
+
+    public static Result buildInsufficientPermissionsResult(String msg) {
+        return buildResult(ResultCode.UNAUTHORIZED, false, msg, null);
+    }
+
+    public static Result buildNotFoundResult(String msg) {
+        return buildResult(ResultCode.NOT_FOUND, false, msg, null);
+    }
+
+}
