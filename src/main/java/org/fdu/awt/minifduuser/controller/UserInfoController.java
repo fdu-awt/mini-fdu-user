b6bea2c3507470db20ec1c97358883521216420f
zmark <351479188@qq.com>
2024-04-19 18:31:13 +0800

    refactor: base-url for UseInfoController

--- src/main/java/org/fdu/awt/minifduuser/controller/UserInfoController.java
+++ src/main/java/org/fdu/awt/minifduuser/controller/UserInfoController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @Validated
 @RestController
-@RequestMapping("/userinfo-service")
+@RequestMapping("/user-service")
 public class UserInfoController {
     private final IUserInfoService userInfoService;
 
