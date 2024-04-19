e77d19185ecf3f8a566274545f79c85e558b65f2
zmark <351479188@qq.com>
2024-04-19 22:32:14 +0800

    data(data.sql): 对接前端，self_image 存模型名称

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
 use mini_fdu;
 
 INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
-    ('Violette', 'violette@example.com', 'violette123','/那维莱特/那维莱特.pmx');
+    ('Violette', 'violette@example.com', 'violette123','那维莱特');
 
 INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
-    ('Rosy', 'rosy@example.com', 'rosy123','/那维莱特/那维莱特.pmx');
+    ('Rosy', 'rosy@example.com', 'rosy123','那维莱特');
 
 INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
-    ('ZMark', 'zmark@example.com', 'zmark123','/荒泷一斗/荒泷一斗.pmx');
+    ('ZMark', 'zmark@example.com', 'zmark123','荒泷一斗');
 
 INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
-    ('chenbuchen', 'chenbuchen@example.com', 'chenbuchen123','/荒泷一斗/荒泷一斗.pmx');
+    ('chenbuchen', 'chenbuchen@example.com', 'chenbuchen123','荒泷一斗');
