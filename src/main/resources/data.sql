0a34c639897d51e59bb8d6d5029944954bb72434
zmark <351479188@qq.com>
2024-04-20 22:28:42 +0800

    data(data.sql): 更新self_image

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,13 @@
 use mini_fdu;
 
 INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
-    ('Violette', 'violette@example.com', 'violette123','那维莱特');
+    ('Violette', 'violette@example.com', 'violette123','花时来信_神里绫华');
 
 INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
-    ('Rosy', 'rosy@example.com', 'rosy123','那维莱特');
+    ('Rosy', 'rosy@example.com', 'rosy123','多莉');
 
 INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
-    ('ZMark', 'zmark@example.com', 'zmark123','荒泷一斗');
+    ('ZMark', 'zmark@example.com', 'zmark123','那维莱特');
 
 INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
     ('chenbuchen', 'chenbuchen@example.com', 'chenbuchen123','荒泷一斗');
