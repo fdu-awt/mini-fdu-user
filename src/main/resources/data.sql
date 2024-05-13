0f9d4094a0d03dc60466bbe94107e6466622faf1
Chen Peiyi <cpyyy_0614@163.com>
2024-05-13 17:59:45 +0800

    feat(data): study_question unfinished

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,22 +1,17 @@
 use mini_fdu;
 
-INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
-    ('Violette', 'violette@example.com', 'violette123','花时来信_神里绫华');
-
-INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
-    ('Rosy', 'rosy@example.com', 'rosy123','多莉');
-
-INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
-    ('ZMark', 'zmark@example.com', 'zmark123','那维莱特');
-
-INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
-    ('chenbuchen', 'chenbuchen@example.com', 'chenbuchen123','荒泷一斗');
+INSERT INTO `user` (`name`, `email`, `password`, `self_image`)
+VALUES ('Violette', 'violette@example.com', 'violette123', '花时来信_神里绫华'),
+       ('Rosy', 'rosy@example.com', 'rosy123', '多莉'),
+       ('ZMark', 'zmark@example.com', 'zmark123', '那维莱特'),
+       ('chenbuchen', 'chenbuchen@example.com', 'chenbuchen123', '荒泷一斗');
 
 INSERT INTO `history_message` (`local_id`, `remote_id`, `content`, `time_stamp`)
-VALUES (1, 2, 'Hello !', '2024-05-01 12:00:00');
+VALUES (1, 2, 'Hello !', '2024-05-01 12:00:00'),
+       (2, 1, 'Hi!', '2024-05-01 12:05:00'),
+       (1, 2, 'How are you today?', '2024-05-01 12:10:00');
 
-INSERT INTO `history_message` (`local_id`, `remote_id`, `content`, `time_stamp`)
-VALUES (2, 1, 'Hi!', '2024-05-01 12:05:00');
-
-INSERT INTO `history_message` (`local_id`, `remote_id`, `content`, `time_stamp`)
-VALUES (1, 2, 'How are you today?', '2024-05-01 12:10:00');
+INSERT INTO `study_question` (`id`, `topic`, `data_id`, `question`, `options`, `answer`)
+VALUES (1, 'history', 1, '复旦公学是哪一年在上海吴淞正式开学的？', 'A. 1902年, B. 1905年, C. 1911年, D. 1908年', 'B'),
+       (2, 'history', 1, '复旦公学的创办人马相伯最初是在哪里借屋为校舍的？',
+        'A. 徐家汇天文台, B. 吴淞提督行辕, C. 震旦学院, D. 天主教堂', 'A');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -26,4 +26,17 @@ CREATE TABLE `history_message`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
+DROP TABLE IF EXISTS `study_question`;
+CREATE TABLE `study_question`
+(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `topic` VARCHAR(255) NOT NULL,
+    `data_id` INT NOT NULL, -- 关联的知识对应的 id
+    `question` VARCHAR(255) NOT NULL,
+    `options` VARCHAR(255) NOT NULL, -- 存储如 "A. 1902年, B. 1905年, C. 1911年, D. 1908年"
+    `answer` CHAR(1) NOT NULL, -- 存储答案的字母，如 'B'
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
 SET FOREIGN_KEY_CHECKS = 1;
