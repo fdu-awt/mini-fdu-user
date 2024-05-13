d008a3e3cd4554ce96bc3bc81cafe5d89e8724cc
zouyi73 <2168603151@qq.com>
2024-05-13 15:35:20 +0800

    update datebase' '

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,29 @@
-SET
-    FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`
 (
-    `id`       INT          NOT NULL AUTO_INCREMENT,
-    `name`     VARCHAR(255) NOT NULL,
-    `email`    VARCHAR(255) NOT NULL,
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `self_image` VARCHAR(255) NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
--- ----------------------------
-SET
-    FOREIGN_KEY_CHECKS = 1;
+DROP TABLE IF EXISTS `history_message`;
+CREATE TABLE `history_message`
+(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `local_id` INT NOT NULL,
+    `remote_id` INT NOT NULL,
+    `content` VARCHAR(255) NOT NULL,
+    `time_stamp` TIMESTAMP NOT NULL,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`local_id`) REFERENCES `user`(`id`) ON DELETE CASCADE,
+    FOREIGN KEY (`remote_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+SET FOREIGN_KEY_CHECKS = 1;
