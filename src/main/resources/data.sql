f1764bed9780ec8c5246a3b8f027c75e33f72b6c
zouyi73 <2168603151@qq.com>
2024-05-14 17:25:24 +0800

    update database design

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,12 +11,3 @@ INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
 
 INSERT INTO `user` (`name`, `email`, `password`,`self_image`) VALUES
     ('chenbuchen', 'chenbuchen@example.com', 'chenbuchen123','荒泷一斗');
-
-INSERT INTO `history_message` (`local_id`, `remote_id`, `content`, `time_stamp`)
-VALUES (1, 2, 'Hello !', '2024-05-01 12:00:00');
-
-INSERT INTO `history_message` (`local_id`, `remote_id`, `content`, `time_stamp`)
-VALUES (2, 1, 'Hi!', '2024-05-01 12:05:00');
-
-INSERT INTO `history_message` (`local_id`, `remote_id`, `content`, `time_stamp`)
-VALUES (1, 2, 'How are you today?', '2024-05-01 12:10:00');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,18 +12,4 @@ CREATE TABLE `user`
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
 
-DROP TABLE IF EXISTS `history_message`;
-CREATE TABLE `history_message`
-(
-    `id` INT NOT NULL AUTO_INCREMENT,
-    `local_id` INT NOT NULL,
-    `remote_id` INT NOT NULL,
-    `content` VARCHAR(255) NOT NULL,
-    `time_stamp` TIMESTAMP NOT NULL,
-    PRIMARY KEY (`id`),
-    FOREIGN KEY (`local_id`) REFERENCES `user`(`id`) ON DELETE CASCADE,
-    FOREIGN KEY (`remote_id`) REFERENCES `user`(`id`) ON DELETE CASCADE
-) ENGINE = InnoDB
-  DEFAULT CHARSET = utf8mb4;
-
 SET FOREIGN_KEY_CHECKS = 1;
