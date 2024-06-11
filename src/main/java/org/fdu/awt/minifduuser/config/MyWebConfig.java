02e508e75c806d82c61512ba3c5b004994eea35c
zmark <351479188@qq.com>
2024-06-11 15:25:55 +0800

    chore: 由网关统一处理跨域问题

--- src/main/java/org/fdu/awt/minifduuser/config/MyWebConfig.java
+++ src/main/java/org/fdu/awt/minifduuser/config/MyWebConfig.java
@@ -2,7 +2,6 @@ package org.fdu.awt.minifduuser.config;
 
 import org.fdu.awt.minifduuser.token.JWTInterceptor;
 import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,16 +10,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @SpringBootConfiguration
 public class MyWebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry corsRegistry) {
-        corsRegistry.addMapping("/**")
-                .allowCredentials(true)
-                .allowedOriginPatterns("*")
-                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
-                .allowedHeaders("*")
-                .maxAge(3600);
-    }
-
     /**
      * token验证
      * 对于除了注册和登录的请求均拦截,拦截转向到 JWTInterceptor
